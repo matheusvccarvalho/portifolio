7ea33abe4ed6fe87f07f7afa5324b09ecb2a4a1e
Matheus Vinicius Carvalho <matheusccarvalhoads@gmail.com>
2026-03-10 23:47:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+particlesJS("particles-js", {
+particles: {
+number: { value: 80 },
+color: { value: "#1f6feb" },
+shape: { type: "circle" },
+opacity: { value: 0.5 },
+size: { value: 3 },
+move: { speed: 2 }
+}
+});
